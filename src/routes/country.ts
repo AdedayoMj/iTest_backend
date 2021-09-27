3516dbf44cbef1a62116d6290647c7f06d74b2cf
Adegboye <macbook@macbooks-MacBook-Air.local>
2021-09-27 19:29:55 +0300

    auth

--- src/routes/country.ts
+++ src/routes/country.ts
@@ -4,7 +4,7 @@ import extractFirebaseInfo from '../middleware/extract_firebase_info';
 
 const router = express.Router();
 
-router.get('/country/:countryQuery',controller.getCountryByName);
-router.get('/',controller.getAll);
+router.get('/country/:countryQuery',extractFirebaseInfo,controller.getCountryByName);
+router.get('/',extractFirebaseInfo,controller.getAll);
 
 export = router;
